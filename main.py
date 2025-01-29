ed7ef64fe948dc72ab3bf360df6b79ddba0b1bd2
Tohrwarneth <Frederic.n.Schneider@web.de>
2025-01-29 23:12:38 +0100

    Init Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    root = Tk()
+    a = Label(root, text="Hello World")
+    a.pack()
+    root.mainloop()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
